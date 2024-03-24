b2dc50ba576a688b544dabf62ad7a263b572931c
Mrbruce <shajedultoky123@gmail.com>
2024-03-24 16:25:05 +0600

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Shajedul Arefin",
+  title: "Aspiring Big data sciectistData science enthusias, Problem solver & Novice Researcher",
+  email: "Shajedul.Arefin@studentambassadors.com",
+  gitHub: "mrbruce778",
+  instagram: "the_bruce_26",
+  linkedIn: "shajedularefin",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "SArefin26",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
